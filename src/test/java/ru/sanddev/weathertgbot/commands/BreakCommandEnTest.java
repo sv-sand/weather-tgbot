bc4c75b21933e9a4c47fc655e89ee4d95edee63d
sand <sve.snd@gmail.com>
2023-09-09 16:31:17 +0300

    classes was renamed

---
+++ src/test/java/ru/sanddev/weathertgbot/commands/BreakCommandEnTest.java
@@ -0,0 +1,37 @@
+package ru.sanddev.weathertgbot.commands;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import ru.sanddev.weathertgbot.BaseCommandTest;
+import ru.sanddev.weathertgbot.bot.LanguageCode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author sand <sve.snd@gmail.com>
+ * @since 30.08.2023
+ */
+
+public class BreakCommandEnTest extends BaseCommandTest {
+
+    @Override
+    public void test() {
+        saveUser("1", "sand", LanguageCode.en);
+
+        Chat chat = newChat("1", "sand");
+        SendMessage message;
+
+        // Change language en -> en
+        botSend(chat, "/lang");
+        message = botAnswer();
+
+        assertEquals(message.getChatId(), chat.getId().toString());
+        assertEquals(message.getText().lines().findFirst().orElseThrow(), "Please select language");
+
+        botSend(chat, "/break");
+        message = botAnswer();
+
+        assertEquals(message.getChatId(), chat.getId().toString());
+        assertEquals(message.getText(), "Command was broke");
+    }
+}
---
+++ src/test/java/ru/sanddev/weathertgbot/commands/StartCommandEnTest.java
@@ -0,0 +1,28 @@
+package ru.sanddev.weathertgbot.commands;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import ru.sanddev.weathertgbot.BaseCommandTest;
+import ru.sanddev.weathertgbot.bot.LanguageCode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author sand <sve.snd@gmail.com>
+ * @since 30.08.2023
+ */
+
+public class StartCommandEnTest extends BaseCommandTest {
+
+    @Override
+    public void test() {
+        saveUser("1", "sand", LanguageCode.en);
+
+        Chat chat = newChat("1", "sand");
+        botSend(chat, "/start");
+
+        SendMessage message = botAnswer();
+        assertEquals(message.getChatId(), chat.getId().toString());
+        assertEquals(message.getText(), "Welcome back, sand");
+    }
+}
