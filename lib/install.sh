191286655f578235fb02123a992225a81ef33dc1
sand <sve.snd@gmail.com>
2023-09-21 22:08:54 +0300

    add weather-client maven dependency

--- lib/install.sh
+++ lib/install.sh
@@ -1,7 +1,7 @@
-mvn install:install-file
-  -Dfile=weather-client.jar
-  -DgroupId=ru.sanddev
-  -DartifactId=weather-client
-  -Dversion=1.0-SNAPSHOT
-  -Dpackaging=jar
+mvn install:install-file \
+  -Dfile=weather-client.jar \
+  -DgroupId=ru.sanddev \
+  -DartifactId=weather-client \
+  -Dversion=1.0-SNAPSHOT \
+  -Dpackaging=jar \
   -DgeneratePom=true
