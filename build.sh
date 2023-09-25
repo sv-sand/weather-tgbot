06a26bcbe3224be55df0d165c0ceae15f89c3b69
sand <sve.snd@gmail.com>
2023-09-25 22:31:43 +0300

    edit run.sh & build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mvn clean
+
+git pull
+
+mvn compile
--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-mvn clean
-git pull
-mvn compile
-
-nohup mvn exec:java -Dexec.mainClass="ru.sanddev.weathertgbot.App" > logs/lohup.out &
+nohup mvn exec:java -Dexec.mainClass="ru.sanddev.weathertgbot.App" > logs/nohup.out &
