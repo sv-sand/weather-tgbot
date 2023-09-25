56992674cdfb6435d7d833d62e82d977cbacf10c
sand <sve.snd@gmail.com>
2023-09-25 22:46:34 +0300

    edit .gitignore & build.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup mvn exec:java -Dexec.mainClass="ru.sanddev.weathertgbot.App" > logs/nohup.out &
+nohup mvn exec:java -Dexec.mainClass="ru.sanddev.weathertgbot.App" > logs/nohup.out 2> logs/nohup.err &
