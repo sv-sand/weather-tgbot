8f97df870530e7978a6d7dafaa74fac56c8a5763
sand <sve.snd@gmail.com>
2023-09-25 22:18:29 +0300

    edit run script

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mvn clean
 git pull
 mvn compile
 
-nohup mvn exec:java -Dexec.mainClass="ru.sanddev.weathertgbot.App"
+nohup mvn exec:java -Dexec.mainClass="ru.sanddev.weathertgbot.App" > logs/lohup.out &
